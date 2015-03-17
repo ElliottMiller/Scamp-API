ed1d73a55e838ede36d721c1ebe6bc401109a947
matv <mateusv@microsoft.com>
2015-03-17 15:41:13 -0700

    v1 of port to Angular and Web API

---
+++ Scamp/App/Scripts/app.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+angular.module('todoApp', ['ngRoute','AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/TodoList", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    adalProvider.init(
+        {
+            tenant: 'dpe1.onmicrosoft.com',
+            clientId: '5480d52a-a26b-47f5-a0a7-c4838f543f7e',
+            extraQueryParameter: 'nux=1',
+            cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        },
+        $httpProvider
+        );
+   
+}]);
